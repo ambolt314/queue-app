b374fa7e84ca2224ed14c2cd80014102adff0116
Andrew Bolt <andrew.bolt@coderus.com>
2020-07-30 14:42:15 +0100

    Initial Commit

---
+++ RabbitMQ tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RabbitMQ tutorial
+//
+//  Created by Andrew Bolt on 30/07/2020.
+//  Copyright © 2020 Andrew Bolt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
